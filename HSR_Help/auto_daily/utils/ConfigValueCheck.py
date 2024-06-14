43cf3d6c14871d385fa930c9934859b76cb44f99
Morning-Maple <morning_maple@foxmail.com>
2024-06-15 01:40:23 +0800

    feat: add some logic to verify legitimacy in config.json.

--- HSR_Help/auto_daily/utils/ConfigValueCheck.py
+++ HSR_Help/auto_daily/utils/ConfigValueCheck.py
@@ -1,10 +1,33 @@
 """
-检查配置文件合法性
+检查配置文件合法性，合法的配置文件内容应该如下（如无特殊说明，默认0为关闭，1为开启）：
+mode: 存储了一个mode对象数组，它定义了副本挑战的相关内容，mode对象的属性请往下看
+waitCheck: 是否开启遗器本和历战余响本挑战成功后暂停查看遗器属性(未实装)
+lazyMan: 是否开启超级懒人模式(未实装)
+useFuel: 是否开启在体力不足的情况下使用燃料(1燃料=60点开拓力)(未实装)
+fuelUseQuantity: 允许使用的燃料数量,可选择为大于等于0的整数(未实装)
+useExtraFuel: 是否开启在体力不足的情况下使用后备开拓力(1点后备开拓力=1点开拓力)(未实装)
+extraFuelUseQuantity: 允许使用的后备开拓力数量，可选择为大于等于0的整数(未实装)，每次使用的数量为60，次数由程序分配
+project: 存了所有可执行的功能，project内的键值对请往下看
+
+mode对象属性：
+mode: 模式大类，接受一个GameModeTypes.py下的ModeType的值，例如侵蚀隧洞
+detail: 模式小类，接受一个GameModeTypes.py下的NZHEJMode或NZHECMode或NZXYMode或QSSDMode或LZYXMode的值，例如冰风套本
+round: 挑战次数，如果设为0则会一直打当前的副本，直到用完所有指定的体力（每次运行只允许有一个副本的挑战次数为0）
+
+project下的键值对：
+weiTuo: 是否执行委托派遣
+email: 是否执行邮件领取
+zhuZhan: 是否执行助战奖励领取
+fuBen: 是否执行副本挑战
+shiXun: 是否执行实训奖励领取
+xunLi: 是否执行无名勋礼任务进度和奖励的领取
 """
 
 import json
 
 import HSR_Help.auto_daily.Types as Types
+
+rounds_limit = False
 
 
 def check_config():
@@ -15,6 +38,37 @@ def check_config():
     """
     with open('config/config.json', 'r', encoding='utf-8') as f:
         config = json.load(f)
+
+    check_dict = {
+        "暂停查看遗器属性": config["waitCheck"],
+        "超级懒狗模式": config["lazyMan"],
+        "使用燃料": config["useFuel"],
+        "可支配的燃料数目": config["fuelUseQuantity"],
+        "使用后备开拓力": config["useExtraFuel"],
+        "可支配的后备开拓力数目": config["extraFuelUseQuantity"],
+    }
+
+    for item_name, item_value in check_dict.items():
+        if item_value is None:
+            raise ValueError(f'{item_name}不存在配置文件中！')
+        if not isinstance(item_value, int):
+            raise ValueError(f'{item_name}的值：{item_value}，不是一个整数！')
+
+        if item_name in ["暂停查看遗器属性", "超级懒狗模式", "使用燃料", "使用后备开拓力"] and item_value not in [0, 1]:
+            raise ValueError(f'{item_name}的值为：{item_value}，它只能在[0, 1]之间取值！')
+        if check_dict["使用燃料"] == 1 and item_name == "可支配的燃料数目" and item_value <= 0:
+            raise ValueError(f'{item_name}的值不能小于等于0')
+        if check_dict["使用后备开拓力"] == 1 and item_name == "可支配的后备开拓力数目" and item_value <= 0:
+            raise ValueError(f'{item_name}的值不能小于等于0')
+
+    functions = config["project"]
+    if functions is None:
+        raise ValueError("配置文件出错，找不到键名：project")
+    for key, value in functions.items():
+        if key not in ['weiTuo', 'email', 'zhuZhan', 'fuBen', 'shiXun', 'xunLi']:
+            raise ValueError(f'找不到键：{key}')
+        if value not in [0, 1]:
+            raise ValueError(f'{value}是不合法的值，它应该为[0, 1]')
 
     project = config["mode"]
     if project is None:
@@ -41,37 +95,44 @@ def modes_check(mode: str, detail: str, rounds: int):
         detail(str): 挑战模式（小类）
         rounds(int): 挑战次数
     """
+    global rounds_limit
     try:
         _ = Types.ModeType[mode]
     except KeyError:
-        raise ValueError('模式选择不在可选范围内')
-    if rounds <= 0:
-        raise ValueError('不合法的挑战次数')
+        raise ValueError(f'模式不合法，{mode}不在可选范围内')
+
+    if rounds == 0:
+        if rounds_limit is True:
+            raise ValueError('挑战轮次不合法，设置了超过两个的轮次为0的副本')
+        rounds_limit = True
+    if rounds < 0:
+        raise ValueError('不合法的挑战次数，值必须大于等于0')
+
     match mode:
         case 'NZHEJ':
             try:
                 _ = Types.NZHEJMode[detail]
             except KeyError:
-                raise ValueError('拟造花萼金中选择了错误的挑战地点')
+                raise ValueError(f'{detail}不属于拟造花萼(金)模式下')
         case 'NZHEC':
             try:
                 _ = Types.NZHECMode[detail]
             except KeyError:
-                raise ValueError('拟造花萼赤中选择了错误的挑战地点')
+                raise ValueError(f'{detail}不属于拟造花萼(赤)模式下')
         case 'NZXY':
             try:
                 _ = Types.NZXYMode[detail]
             except KeyError:
-                raise ValueError('凝滞虚影中选择了错误的挑战地点')
+                raise ValueError(f'{detail}不属于凝滞虚影模式下')
         case 'QSSD':
             try:
                 _ = Types.QSSDMode[detail]
             except KeyError:
-                raise ValueError('侵蚀隧洞中选择了错误的挑战地点')
+                raise ValueError(f'{detail}不属于侵蚀隧洞模式下')
         case 'LZYX':
             try:
                 _ = Types.LZYXMode[detail]
             except KeyError:
-                raise ValueError('历战余响中选择了错误的挑战地点')
+                raise ValueError(f'{detail}不属于历战余响模式下')
         case _:
-            raise ValueError('模式选择不在可选范围内')
+            raise ValueError(f'{mode}不在可选范围内，你是怎么绕过前面的检测的？')
